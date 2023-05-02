4be9c6e102d0e4e9544626e385600dbb0ca0413a
Savindu Bandara <SavinduB@99x.io>
2023-05-02 10:08:59 +0530

    Initial commit

---
+++ Controllers/PersonsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var persons = new[]
+            {
+                "Savindu",
+                "Bandara",
+                "Pasi boy",
+                "Archana"
+            }; 
+            return Ok(persons);
+        }
+    }
+}
